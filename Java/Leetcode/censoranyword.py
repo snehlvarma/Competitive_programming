c619be34a234b1bb06b196e626da628026c9b91c
snehil verma <37077644+snehlvarma@users.noreply.github.com>
2021-06-12 13:29:12 +0530

    Create censoranyword.py
    Censor all the words given in list

---
+++ Java/Leetcode/censoranyword.py
@@ -0,0 +1,12 @@
+words = ["fat" , "bitch" , "donkey" , "retard", "idiot"]
+
+with open(sample.txt) as f:
+   content = f.read()
+  
+for word in words:
+  content = content.replace( word, #$&#^^$^)
+                            
+  with open(sample.txt, "w") as f:
+     f.write(content)                       
+                            
+                            
