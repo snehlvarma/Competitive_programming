80a82a5eecc542d45b3570bd112567bef7120b4d
snehil verma <37077644+snehlvarma@users.noreply.github.com>
2021-06-12 13:22:45 +0530

    Create consorword.py
    censor a word in python read and write it

---
+++ Java/Leetcode/consorword.py
@@ -0,0 +1,9 @@
+with open(sample.txt) as f:
+  content f.read()
+  
+content = content.replace(donkey , #$^%@%^)
+                          
+with open("sample.txt" , "w") as f :
+     f.write(content)
+                          
+            
